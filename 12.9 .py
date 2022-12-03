b58b825a92a2b9aac8fc43fb67522978c2a6cbe7
PawanKumar24 <112110136+PawanKumar24@users.noreply.github.com>
2022-12-03 12:58:41 +0500

    Add files via upload

---
+++ 12.9 .py
@@ -0,0 +1,21 @@
+# Pawan Kumar
+# ID: 2046222
+age = 0
+
+mylist=[]
+temp = ""
+
+while temp!="-1":
+    temp  = input()
+    if temp!="-1":
+        mylist.append(temp)
+
+for item in mylist:
+    temp = item.split(" ")
+    age = temp[1]
+    inputName = temp[0]
+    try:
+        age = int(age)
+        print(f"{inputName} {(age + 1)}")
+    except ValueError as ve:
+        print(f"{inputName} 0")
